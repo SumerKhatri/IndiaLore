eaeec5b247b74bef4b5e6769a4ef4884de28b9c6
Sumer <sumerkhatri12@gmail.com>
2023-04-27 21:28:01 +0530

    Added Info Page

--- app/src/main/java/com/example/indialore/activities/InfoActivity.java
+++ app/src/main/java/com/example/indialore/activities/InfoActivity.java
@@ -3,6 +3,7 @@ package com.example.indialore.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.indialore.R;
 
@@ -12,5 +13,8 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        TextView tv=findViewById(R.id.textView8);
+        String s="The team of IndiaLore believes that it is very important to service all the needs of its customers to provide them what they really want with a hassle-free and a great user experience, also keeping in mind that our seller's family is equally benefited and achieve higher profits when they are enrolled with IndiaLore.\nIndiaLore's priority is the security and privacy of their users including both our valueable customer's as well as our seller family. Thus, to ensure that we provide smart contract-based architecture for buying and selling of the products and, also assuring the authenticity of the traditional product being sold.\n";
+        tv.setText(s);
     }
 }
